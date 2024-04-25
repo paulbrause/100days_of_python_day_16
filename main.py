40f0c44eb120905fe96ec457c748908fd250b1e7
paulbrause <paulbrause@googlemail.com>
2024-04-25 16:24:42 +0200

    Coffee Maker (OOP)

---
+++ main.py
@@ -0,0 +1,26 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+is_on = True
+
+menu = Menu()
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+
+while is_on:
+    options = menu.get_items()
+    command = input(f"What would you like? ({options}): ").lower()
+
+    if command == "off":
+        is_on = False
+    elif command == "report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(command)
+        if drink is not None:
+            if coffee_maker.is_resource_sufficient(
+                drink
+            ) and money_machine.make_payment(drink.cost):
+                coffee_maker.make_coffee(drink)
